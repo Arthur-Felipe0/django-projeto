956ebe54afaae48c162dd6386f7e5cd90a237051
Arthur Felipe <arthur.fcardoso05@gmail.com>
2023-09-13 17:24:53 -0300

    .

--- site/locallibrary/catalog/admin.py
+++ site/locallibrary/catalog/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from catalog.models import Author, Genre, Book, BookInstance
 
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+#admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
 
+#Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
+#Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
 
+#Register the admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+#Register the Admin calsses for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
